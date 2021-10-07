d307078cd1bd030b00cfaa231171739cdb184bef
Adam <adam.midtt@gmail.com>
2021-10-07 18:56:47 +0200

    commit

---
+++ ang1remind/src/app/services/task.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  constructor() {}
+
+  getTasks(): Observable<Task[]>{
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
